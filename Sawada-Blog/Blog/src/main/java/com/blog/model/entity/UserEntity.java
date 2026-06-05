c4ac2fbd32886f19fc24694789f5a947f8a18cca
澤田純弥 <sawadajunya@SJMacBook-Air.local>
2026-06-05 19:40:54 +0900

    UserEntityとUserDaoの作成

---
+++ Sawada-Blog/Blog/src/main/java/com/blog/model/entity/UserEntity.java
@@ -0,0 +1,72 @@
+package com.blog.model.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long userId;
+	private String userName;
+	private String email;
+	private String password;
+	private LocalDateTime registerDate;
+
+	public UserEntity() {
+
+	}
+
+	public UserEntity(String userName, String email, String password, LocalDateTime registerDate) {
+		super();
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+		this.registerDate = registerDate;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public LocalDateTime getRegisterDate() {
+		return registerDate;
+	}
+
+	public void setRegisterDate(LocalDateTime registerDate) {
+		this.registerDate = registerDate;
+	}
+
+}
