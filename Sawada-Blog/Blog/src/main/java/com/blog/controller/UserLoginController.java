c2b6c0b985c4e0a0ea94d82e13e73cff84b99cb0
澤田純弥 <sawadajunya@SJMacBook-Air.local>
2026-06-07 23:58:40 +0900

    UserLoginController、UserRegisterController、loginAccount(UserService)を作成しました。

--- Sawada-Blog/Blog/src/main/java/com/blog/controller/UserLoginController.java
+++ Sawada-Blog/Blog/src/main/java/com/blog/controller/UserLoginController.java
@@ -1,5 +1,23 @@
 package com.blog.controller;
 
-public class UserLoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+public class UserLoginController {
+	@GetMapping("/user/login")
+	public String getUserLoginPage() {
+		return "login.html";
+	}
+	
+	@PostMapping("/user/login/process")
+	public String login(@RequestParam String email, @RequestParam String password) {
+		if(userService.loginAccount(email, password) == null) {
+			return "redirect:/user/blog/list";
+		} else {
+			return "redirect:/user/login";
+		}
+	}
 }
--- Sawada-Blog/Blog/src/main/java/com/blog/controller/UserRegisterController.java
+++ Sawada-Blog/Blog/src/main/java/com/blog/controller/UserRegisterController.java
@@ -1,5 +1,28 @@
 package com.blog.controller;
 
-public class UserRegisterController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.blog.service.UserService;
+
+@Controller
+public class UserRegisterController {
+@Autowired
+private UserService userService;
+	@GetMapping("/uer/register")
+	public String getUserRegisterPage() {
+		return "register.html";
+	}
+	
+	@PostMapping("/user/register/process")
+	public String register(@RequestParam String userName, @RequestParam String email, @RequestParam String password) {
+		if(UserService.createAccount(userName, email, password)) {
+			return "redirect:/user/login";
+		}else {
+			return "redirect:/user/login";
+		}
+	}
 }
